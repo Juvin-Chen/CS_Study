2107434b3129e8d650e1bf1494071e57723979ab
Juvin <hdcqW@outlook.com>
2026-02-07 23:23:24 +0800

    Course Design

--- 课设GUI_程序设计实践/src/Skill.h
+++ 课设GUI_程序设计实践/src/Skill.h
@@ -1,7 +1,7 @@
 #pragma once
-#include <cmath>
-#include "graphics.h"
 #include "Rabbit.h"
+#include "graphics.h"
+#include <cmath>
 
 struct Skill {
     int x1, y1, x2, y2;
@@ -9,7 +9,7 @@ struct Skill {
     bool active = false;
 };
 
-inline void trigger(Rabbit& rabbit, Skill& skill, int vkKey) {
+inline void trigger(Rabbit &rabbit, Skill &skill, int vkKey) {
     if (rabbit.canUseSkill() && (GetAsyncKeyState(vkKey) & 0x8000)) {
         skill.active = true;
         skill.timer = 15;
@@ -20,16 +20,23 @@ inline void trigger(Rabbit& rabbit, Skill& skill, int vkKey) {
     }
 }
 
-inline void skilleff(const Rabbit& a, Rabbit& b, Skill& skill) {
-    if (!skill.active) return;
+inline void skilleff(const Rabbit &a, Rabbit &b, Skill &skill) {
+    if (!skill.active)
+        return;
+
     setlinecolor(RGB(255, 255, 0));
     setlinestyle(PS_SOLID, 2);
+
     line(skill.x1, skill.y1, skill.x2, skill.y2);
-    float A = skill.y2 - skill.y1;  //AX+BY+C=0
+
+    float A = skill.y2 - skill.y1; // AX+BY+C=0
     float B = skill.x1 - skill.x2;
     float C = skill.x2 * skill.y1 - skill.x1 * skill.y2;
     float d = fabs(A * b.x + B * b.y + C) / sqrt(A * A + B * B);
+
     float judge = (b.x - a.x) * a.dir.x + (b.y - a.y) * a.dir.y;
-    if (d < b.radius() && judge>0) b.alive = false;
-    if (--skill.timer <= 0) skill.active = false;
+    if (d < b.radius() && judge > 0)
+        b.alive = false;
+    if (--skill.timer <= 0)
+        skill.active = false;
 }
--- 课设GUI_程序设计实践/src/main.cpp
+++ 课设GUI_程序设计实践/src/main.cpp
@@ -1,12 +1,13 @@
-#include<graphics.h>    
-#include<conio.h>       
-#include<windows.h>     
-#include<vector>        
-#include<cmath>        
-#include<ctime>         
-#include<fstream>
-#include"Rabbit.h"
-#include"Skill.h"
+#include "Rabbit.h"
+#include "Skill.h"
+#include <cmath>
+#include <conio.h>
+#include <ctime>
+#include <fstream>
+#include <graphics.h>
+#include <vector>
+#include <windows.h>
+
 using namespace std;
 
 constexpr int WIDTH = 900, HEIGHT = 900;
@@ -14,9 +15,9 @@ constexpr int drinks_sum = 120;
 constexpr int R = 50;
 constexpr int skill_time = 15;
 
-struct L {   //location
+struct L { // location
     int x, y;
-    L(int a, int b) :x(a), y(b) {}
+    L(int a, int b) : x(a), y(b) {}
 };
 
 IMAGE imgBackground, imgWin1, imgWin2;
@@ -26,7 +27,7 @@ void loadImages() {
     loadimage(&imgWin2, _T("../img/2win.png"), WIDTH, HEIGHT);
 }
 
-void move(Vec& dir, Rabbit& r) {
+void move(Vec &dir, Rabbit &r) {
     if (dir.x || dir.y) {
         dir.abc();
         r.x += dir.x * r.speed();
@@ -41,12 +42,10 @@ int main() {
     loadImages();
     BeginBatchDraw();
 
-    auto fun = [](int x, int y) {
-        return x + rand() % (y - x + 1);
-        };
+    auto fun = [](int x, int y) { return x + rand() % (y - x + 1); };
     vector<L> drinks;
-    for (int i = 0;i < drinks_sum;i++)
-        drinks.push_back({ fun(20,WIDTH - 20), fun(20, HEIGHT - 20) });
+    for (int i = 0; i < drinks_sum; i++)
+        drinks.push_back({fun(20, WIDTH - 20), fun(20, HEIGHT - 20)});
 
     Rabbit r1(100, 100), r2(800, 800);
     Skill skill1, skill2;
@@ -57,38 +56,46 @@ int main() {
         framecount++;
 
         Vec d1(0, 0);
-        if (GetAsyncKeyState(VK_LEFT))  d1.x = -1;
-        if (GetAsyncKeyState(VK_RIGHT)) d1.x = 1;
-        if (GetAsyncKeyState(VK_UP))    d1.y = -1;
-        if (GetAsyncKeyState(VK_DOWN))  d1.y = 1;
+        if (GetAsyncKeyState(VK_LEFT))
+            d1.x = -1;
+        if (GetAsyncKeyState(VK_RIGHT))
+            d1.x = 1;
+        if (GetAsyncKeyState(VK_UP))
+            d1.y = -1;
+        if (GetAsyncKeyState(VK_DOWN))
+            d1.y = 1;
         Vec d2(0, 0);
-        if (GetAsyncKeyState('A')) d2.x = -1;
-        if (GetAsyncKeyState('D')) d2.x = 1;
-        if (GetAsyncKeyState('W')) d2.y = -1;
-        if (GetAsyncKeyState('S')) d2.y = 1;
+        if (GetAsyncKeyState('A'))
+            d2.x = -1;
+        if (GetAsyncKeyState('D'))
+            d2.x = 1;
+        if (GetAsyncKeyState('W'))
+            d2.y = -1;
+        if (GetAsyncKeyState('S'))
+            d2.y = 1;
 
         move(d1, r1);
         move(d2, r2);
         trigger(r1, skill1, VK_RETURN);
         trigger(r2, skill2, 'F');
 
-        for (int i = (int)drinks.size() - 1;i >= 0;i--) {
-            L& f = drinks[i];
+        for (int i = (int)drinks.size() - 1; i >= 0; i--) {
+            L &f = drinks[i];
             if (hypot(r1.x - f.x, r1.y - f.y) < r1.radius() + 12) {
                 r1.drinks++;
                 drinks.erase(drinks.begin() + i);
-            }
-            else if (hypot(r2.x - f.x, r2.y - f.y) < r2.radius() + 12) {
+            } else if (hypot(r2.x - f.x, r2.y - f.y) < r2.radius() + 12) {
                 r2.drinks++;
                 drinks.erase(drinks.begin() + i);
             }
         }
 
-        auto limit = [&](Rabbit& r) {
+        auto limit = [&](Rabbit &r) {
             r.x = min(max(r.x, (float)r.radius()), WIDTH - r.radius());
             r.y = min(max(r.y, (float)r.radius()), HEIGHT - r.radius());
-            };
-        limit(r1); limit(r2);
+        };
+        limit(r1);
+        limit(r2);
 
         cleardevice();
         putimage(0, 0, &imgBackground);
@@ -96,9 +103,9 @@ int main() {
         skilleff(r1, r2, skill1);
         skilleff(r2, r1, skill2);
 
-        for (auto& f : drinks) {
+        for (auto &f : drinks) {
             setfillcolor(RGB(144, 238, 144));
-            int drink[8] = { f.x - 8, f.y - 5, f.x + 8, f.y - 5, f.x + 5, f.y + 15, f.x - 5, f.y + 15 };
+            int drink[8] = {f.x - 8, f.y - 5, f.x + 8, f.y - 5, f.x + 5, f.y + 15, f.x - 5, f.y + 15};
             fillpoly(4, drink);
             setfillcolor(RGB(139, 69, 19));
             fillrectangle(f.x - 2, f.y - 15, f.x + 2, f.y - 5);
@@ -109,20 +116,23 @@ int main() {
 
         FlushBatchDraw();
         Sleep(30);
-        if (_kbhit() && _getch() == 27) break;
+        if (_kbhit() && _getch() == 27)
+            break;
     }
 
     cleardevice();
-    if (!r2.alive) putimage(0, 0, &imgWin1);
-    else if (!r1.alive) putimage(0, 0, &imgWin2);
+    if (!r2.alive)
+        putimage(0, 0, &imgWin1);
+    else if (!r1.alive)
+        putimage(0, 0, &imgWin2);
     FlushBatchDraw();
-
 
     ofstream file("best_kos.txt", ios::app);
     file << "This game used " << framecount << " frames.\n";
     file.close();
 
-    while (!_kbhit()) Sleep(100);
+    while (!_kbhit())
+        Sleep(100);
     _getch();
     closegraph();
     return 0;
